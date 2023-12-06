f6bd03e659463c0180a673dd13529f0970b532f0
mariahgc <maria.17aac16@gmail.com>
2023-12-06 16:51:47 -0300

    calculadora_finalizada_programação

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 function valor(value) {
-    document.getElementById('display').value += value;
+    document.getElementById('display').value += value; // o valor, operação do usuario e o outro valor escolhido //
 }
 function limpar() {
-    document.getElementById('display').value = ' ';
+    document.getElementById('display').value = ' '; // vazio para limpar o campo de digitação //
 }
 function total() {
-    document.getElementById('display').value = eval(document.getElementById('display').value);
-
+    document.getElementById('display').value = eval(document.getElementById('display').value); // retorno do resultado //
 }
